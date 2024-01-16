11d16853a2646e22b7d199423898da383a7d78e1
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2024-01-16 22:58:00 +0100

    feat: Add database connection module and update server.js
    - Introduce dbConnection.js in the config folder for MongoDB connection setup
    
    - Update server.js to utilize the new database connection module

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,28 @@
+// Load environment variables from a .env file
 require('dotenv').config()
 
+// Import necessary modules
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
+const connectDB = require('./config/dbConnection')
 
+// Define the port to be used or default to 3500
 const PORT = process.env.PORT || 3500
 
-// Start the Express server
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+// Create an instance of the Express application
+const app = express()
+
+// Connect to the database
+connectDB()
+const db = mongoose.connection
+
+// Event handler for database connection errors
+db.on('error', (error) => console.log(error))
+
+// Event handler for successful database connection
+db.once('open', () => {
+    console.log('Connected to Database')
+
+    // Start the Express server
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
